40ae45dfab077b1c7932b0221354075c0e2c8641
PeterSlakhorst <peter_slakhorst@hotmail.com>
2020-10-05 11:46:28 +0200

    ..

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import math
 # comment toegevoegd in commit 2
 # comment toegevoegd in github commit 3
+# comment toegevoegd branch_peter
 
 print("De waarde van pi: ", math.pi)
 print("De waarde van e: ", math.e)
