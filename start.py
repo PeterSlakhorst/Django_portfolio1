0e9669ae4c15e989e3853306c15e6f03918ea6c2
PeterSlakhorst <peter_slakhorst@hotmail.com>
2020-10-06 11:56:21 +0200

    Django commit

--- start.py
+++ start.py
@@ -1,6 +1,31 @@
 import math
+import pandas
+import numpy as np
+
+class Point():
+
+    def __init__(self, input_x, input_y):
+        self.x = input_x
+        self.y = input_y
+
 # comment toegevoegd in commit 2
 # comment toegevoegd in github commit 3
 
 print("De waarde van pi: ", math.pi)
 print("De waarde van e: ", math.e)
+
+
+math.sin
+
+
+s = set()
+
+for i in range(10):
+    s.add(i)
+
+
+
+print(f"Mijn  verzameling s is {len(s)} lang.")
+
+punt = Point(1,2)
+print(punt.x)
