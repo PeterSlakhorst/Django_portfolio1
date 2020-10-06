b7220bc6645228b16926ef36915c43d485438b64
PeterSlakhorst <peter_slakhorst@hotmail.com>
2020-10-06 12:26:47 +0200

    commit Phase1 app

--- Manhattan/Manhattan/settings.py
+++ Manhattan/Manhattan/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Phase1',   # added the app for Phase1
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
